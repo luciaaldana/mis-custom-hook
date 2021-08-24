4bd0f1c6fe498c5e9ab8a25e3d15a91dbebd30c1
Lucia Aldana Castillo <luciaaldanacastillo@gmail.com>
2021-08-24 11:38:01 -0300

    Primer commit

---
+++ useCouter.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+export const useCouter = (initialState = 10) => {
+  const [counter, setCounter] = useState(initialState);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+  const reset = () => {
+    setCounter(initialState);
+  };
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
+  return {
+    counter,
+    increment,
+    reset,
+    decrement,
+  };
+};
